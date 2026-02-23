7a736d6734575a374f627e3b047745fb01065aea
JANHVI GUPTA <150956981+janhvigupta01@users.noreply.github.com>
2026-02-23 09:52:08 +0530

    Add left view function for binary tree
    Implemented the left view of a binary tree using level order traversal. The function returns a vector containing the leftmost nodes at each level.

---
+++ Left View of Binary Tree.cpp
@@ -0,0 +1,44 @@
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<int> leftView(Node *root) {
+        // code here
+         vector<int>a;
+        vector<vector<int>>ans;
+         if(!root)return a;
+        queue<Node*>q;
+        q.push(root);
+        
+        while(!q.empty()){
+            int s=q.size();
+            vector<int>temp;
+            for(int i=0;i<s;i++){
+                Node* curr=q.front();
+                temp.push_back(curr->data);
+                q.pop();
+                if(curr->left != NULL) q.push(curr->left);
+                if(curr->right != NULL) q.push(curr->right);
+            }
+            ans.push_back(temp);
+        }
+        for(vector<int>it:ans){
+            int last=it[0];
+            a.push_back(last);
+        }
+        return a;
+    }
+};
