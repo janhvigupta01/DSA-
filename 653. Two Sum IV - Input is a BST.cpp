017b9c78df5c072f2ae8767efc456baba5b65f85
JANHVI GUPTA <150956981+janhvigupta01@users.noreply.github.com>
2026-02-24 10:09:25 +0530

    Implement solution for Two Sum IV in a BST

---
+++ 653. Two Sum IV - Input is a BST.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+bool helper(TreeNode* root,unordered_set<int>& st,int k){
+    if(root==NULL) return false;
+    if(st.find(k-root->val)!=st.end()) return true;
+    st.insert(root->val);
+    return helper(root->left,st,k) || helper(root->right,st,k);
+}
+    bool findTarget(TreeNode* root, int k) {
+        unordered_set<int>st;
+        return helper(root,st,k);
+    }
+};
