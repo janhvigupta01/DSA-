6c856d206acea08fcde64cce330dafc620833acf
JANHVI GUPTA <150956981+janhvigupta01@users.noreply.github.com>
2026-02-26 15:25:31 +0530

    Add DFS implementation in Solution class
    Implement depth-first search (DFS) for a graph.

---
+++ dfs of a graph.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void dfsHelper(int node, vector<vector<int>>& adj, vector<int>& vis, vector<int>& ls) {
+        vis[node] = 1;
+        ls.push_back(node);
+
+        for (auto it : adj[node]) {
+            if (!vis[it]) {
+                dfsHelper(it, adj, vis, ls);
+            }
+        }
+    }
+
+    vector<int> dfs(vector<vector<int>>& adj) {
+        int n = adj.size();
+        vector<int> vis(n, 0);
+        vector<int> ls;
+
+        dfsHelper(0, adj, vis, ls);   // start DFS from node 0
+        return ls;
+    }
+};
